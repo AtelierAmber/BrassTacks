e94ce2f15ee8b92e4413fccc9a5753c4a2e9d9f2
Amber <aqscode@gmail.com>
2025-12-03 08:40:50 -0700

    Fixed tech dependencies with hardened hull

--- data-updates.lua
+++ data-updates.lua
@@ -80,7 +80,7 @@ if parts.experimental then
   rm.RemoveIngredient("inserter", "aluminum-plate", 1)
   rm.RemoveIngredient("long-handed-inserter", "aluminum-plate", 1)
 
-  tf.addPrereq(mods["aai-industry"] and "basic-fluid-handling" or "fluid-handling", "hardened-hull")
+  --tf.addPrereq(mods["aai-industry"] and "basic-fluid-handling" or "fluid-handling", "hardened-hull")
   if not mods["Krastorio2"] then
     rm.ReplaceIngredient("storage-tank", "steel-plate", "hardened-hull", 5)
     rm.RemoveIngredient("storage-tank", "bronze-plate", 10)
